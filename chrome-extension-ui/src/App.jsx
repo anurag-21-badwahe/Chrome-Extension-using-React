5bfb84088d0be85ca6dcfc4f9d235f9bb79a5e18
anurag <anuragbadwahe@gmail.com>
2024-02-23 00:56:04 +0530

    Added state to Button

--- chrome-extension-ui/src/App.jsx
+++ chrome-extension-ui/src/App.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+
+
 function App() {
+  const [btn, setBtn] = useState(false)
   function toggleGrayscale() {
+    setBtn(!btn);
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const tab = tabs[0];
       if (tab && tab.id !== undefined) {
@@ -13,7 +18,7 @@ function App() {
   return (
     <div className="App">
       <h1>Vanish Colors</h1>
-      <button className = ".btn" onClick={toggleGrayscale} style = {{"backgroundColor" : "#89CFF0","marginLeft" : "55px"}}>Remove Color</button>
+      <button className = ".btn" onClick={toggleGrayscale} style = {{"backgroundColor" : "#89CFF0","marginLeft" : "55px"}}>{btn?"Remove Color":"Regenerate Color"}</button>
     </div>
   );
 }
